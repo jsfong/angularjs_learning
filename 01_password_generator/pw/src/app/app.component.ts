7040fd68923378bb72fab987c9988cc90c2d3a4c
jsfong <jiunnshian.fong@thalesgroup.com>
2021-03-16 17:21:03 +0800

    Finish up 01 Pw

--- 01_password_generator/pw/src/app/app.component.ts
+++ 01_password_generator/pw/src/app/app.component.ts
@@ -7,9 +7,54 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   //Class property
+  length = 0;
   password = ''; //Constructor will init to empty value
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+
+  onChangeLength(value: string){
+    const parsedValue = parseInt(value);
+
+    if(!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
+
+  onChangeUseLetters(){
+    this.includeLetters = !this.includeLetters;  
+  }
+
+  onChangeUseNumbers(){
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols(){
+    this.includeSymbols = !this.includeSymbols;
+  }
 
   onButtonClick(){
-    this.password = 'MY PASSWORD!!!';
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+    if(this.includeLetters){
+      validChars += letters;
+    }
+    if(this.includeNumbers){
+      validChars += numbers;
+    }
+    if(this.includeSymbols){
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+    for (let i=0; i< this.length; i++){
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
   }
+
 }
