483f18ced9cf41d852fd30afa3d4b1726c613e42
jsfong <jsfong5105@gmail.com>
2021-03-17 00:01:22 +0800

    Finalise card

--- cards/src/app/app.component.ts
+++ cards/src/app/app.component.ts
@@ -5,6 +5,26 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    { 
+      title: 'Neat Tree',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this neat tree today'
+    },
+    { 
+      title: 'Snowy Mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'mountainlover',
+      content: 'Here is a picture of a snowy mountain'
+    },
+    { 
+      title: 'Mountain Biking',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'biking222',
+      content: 'I did some biking today'
+    }
+  ]
 }
